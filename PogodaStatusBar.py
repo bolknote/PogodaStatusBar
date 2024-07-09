43af41c25fd10c05dcd44ed64aff6f5f503fe527
Evgeny Stepanischev <imbolk@gmail.com>
2024-07-09 13:50:01 +0300

    Fixed region detection, Yandex.Pogoda → Gismeteo

--- PogodaStatusBar.py
+++ PogodaStatusBar.py
@@ -1,39 +1,32 @@
 import sublime
 import sublime_plugin
 import urllib.request
+import urllib.parse
 import xml.etree.ElementTree as ET
 import re
+import json
 
 POGODASTATUSBAR_SETTING_FILE = 'PogodaStatusBar.sublime-settings'
 
 
 class PogodaStatusBar(sublime_plugin.EventListener):
-    # https://tech.yandex.ru/weather/doc/dg/concepts/translations-docpage/
     # Weather icons
     _icons = {
-        "clear": "🌞",
-        "mostly-clear": "🌤",
-        "partly-cloudy": "🌤",
-        "overcast": "🌥",
-        "partly-cloudy-and-light-rain": "🌦",
-        "partly-cloudy-and-rain": "🌧",
-        "overcast-and-rain": "🌧",
-        "overcast-thunderstorms-with-rain": "⛈",
-        "cloudy": "🌤",
-        "cloudy-and-light-rain": "🌦",
-        "overcast-and-light-rain": "🌦",
-        "cloudy-and-rain": "🌧",
-        "overcast-and-wet-snow": "🌨",
-        "partly-cloudy-and-light-snow": "🌨",
-        "partly-cloudy-and-snow": "🌨",
-        "overcast-and-snow": "🌨",
-        "cloudy-and-light-snow": "🌨",
-        "overcast-and-light-snow": "🌨",
-        "cloudy-and-snow": "🌨"
+        "🌞": {"d"},
+        "🌙": {"n"},
+        "☁️": {"n", "c"},
+        "🌧": {"rs", "c", "r"},
+        "🌤": {"d", "c"},
+        "🌦": {"d", "r"},
+        "🌩️": {"c", "st"},
+        "⛈️": {"c", "st", "r"},
+        "🌨": {"c", "s"},
+        "💨": {"mist"},
+        "⚡️": {"st"},
     }
 
     # Traffic level icons
-    _ticons = {'green': '🍏', 'yellow': '🍋', 'red': '🍅'}
+    _ticons = {'green': '🟢', 'yellow': '🟡', 'red': '🔴'}
 
     # Settings: update interval
     _updateInterval = None
@@ -60,7 +53,6 @@ class PogodaStatusBar(sublime_plugin.EventListener):
             self._updateInterval = settings.get('update_interval', 600)
             self._template = settings.get('template', None)
             self._region = self._getRegion() or '0'
-
             self._updateData()
             self._startTimer()
 
@@ -71,9 +63,9 @@ class PogodaStatusBar(sublime_plugin.EventListener):
     # Get current region
     def _getRegion(self):
         try:
-            url = "https://yandex.ru/pogoda/"
+            url = "https://yandex.ru/tune/geo/"
             content = urllib.request.urlopen(url).read()
-            return re.search(r'/pogoda/(\d+)', str(content)).group(1)
+            return re.search(r'"region":\s*(\d+)', str(content)).group(1)
         except (IOError, AttributeError):
             return None
 
@@ -88,7 +80,7 @@ class PogodaStatusBar(sublime_plugin.EventListener):
 
         sublime.set_timeout_async(lambda: self._startTimer(), timeout * 1e3)
 
-    # Get current weather and traffic level
+    # Get current traffic level
     def _getData(self):
         try:
             url = "https://export.yandex.ru/bar/reginfo.xml?region=" + self._region
@@ -97,28 +89,53 @@ class PogodaStatusBar(sublime_plugin.EventListener):
         except (IOError, ET.ParseError):
             return None
 
-    # Get weather icon from XML element
-    def _getStatus(self, el):
-        day_part = el.findall('day_part')[0]
+    # Get weather Unicode icon
+    def _getStatus(self, coded_weather):
+        codes = {x.strip("0123456789") for x in coded_weather.split(".")}
 
-        return self._icons.get(
-            day_part.find('weather_code').text,
-            day_part.find('weather_type').text
-        )
+        max_icon = None
+        max_score = 0
+
+        for icon, code in self._icons.items():
+            result = codes & code
+            score = sum(len(x) for x in result)
+            if score > max_score:
+                max_icon, max_score = icon, score
+
+        return max_icon
 
     # Get traffic level icon from XML element
     def _getTrafficIcon(self, el):
         return self._ticons[el.find('icon').text]
+
+    # Get Gismeteo region by city title
+    def _getGismeteoRegion(self, city_title):
+        url = "https://www.gismeteo.ru/rmq/search/%s/1/" % urllib.parse.quote_plus(city_title)
+        content = urllib.request.urlopen(url).read()
+        json_content = json.loads(content)
+        return json_content['data'][0]['id']
+
+    # Get Gistemeto forecast data
+    def _getGismeteoForecast(self, region):
+        try:
+            url = f'https://services.gismeteo.ru/inform-service/inf_chrome/forecast/?lang=en&city={region}'
+            content = urllib.request.urlopen(url).read()
+            return ET.fromstring(content)
+        except (IOError, ET.ParseError):
+            return None
+
 
     # Update statusbar string cache
     def _updateData(self):
         xml = self._getData()
 
         if xml is not None:
-            weather = xml.find('weather').find('day')
-            title = weather.find('title').text
-            status = self._getStatus(weather)
-            temp = weather.findall('day_part')[0].find('temperature').text
+            title = xml.find('region').find('title').text
+            gm_xml = self._getGismeteoForecast(self._getGismeteoRegion(title))
+
+            weather = gm_xml.findall('./location/fact/values')[0]
+            status = self._getStatus(weather.attrib['icon'])
+            temp = weather.attrib['t']
 
             try:
                 traffic = xml.find('traffic').find('region')
